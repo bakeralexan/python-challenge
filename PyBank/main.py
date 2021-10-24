ae610b046a91147dc97c28a097cb336828189a2f
bakera <bakera@alumni.usc.edu>
2021-10-24 00:31:47 -0700

    Updated code for pybank

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,23 +10,20 @@ with open(csv_path, "r") as csvfile:
     print("----------------------------")
 
     print(f"CSV: {csv_header}")
-    #def budget_stats(budget_data):
+
     
     current_month = 0
     previous_month = 0
     max_increase = 0
-    max_month = ""
+    max_month = []
     max_decrease = 0
-    min_month = ""
+    min_month = []
     delta_list = []
     budget_data = []
-    #put flat amounts for list
-    #can try to sort
+
     row = ()
     total = 0
-    # keep track of a day that you have the most amount from in a loop if today's value is higher than yesterday's value then 
-    #the hold of the amount of that day and the day
-    #keeping track of the highest positive and the lowest negative need another variable to keep track of the dates along with the delta
+
     for row in csvreader :
         lineNum = csvreader.line_num
         budget_data.append(row)
@@ -39,9 +36,8 @@ with open(csv_path, "r") as csvfile:
 
         previous_month = int(current_month)
         delta_list.append(delta)
-        max_increase = max(delta_list)  
-        max_decrease = min(delta_list)
 
+        
         if delta > max_increase:
             max_increase = delta
             max_month = dates
@@ -51,9 +47,9 @@ with open(csv_path, "r") as csvfile:
             min_month = dates
 
 
-    delta_list.pop(0) #print(f"Delta_list: {delta_list}") was the function used to that the first value needed to be popped
+    delta_list.pop(0)
 
-    print(f"Average Change : $", sum(delta_list)/(lineNum-1))
+    print(f"Average Change : $", "{:.2f}".format(sum(delta_list)/(lineNum-1)))
     
     print(f"Total Months: {lineNum}")
     print(f"Total: ${total}")
